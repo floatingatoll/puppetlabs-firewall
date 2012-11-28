190d3291e96f351aecfbfb10dd96e5517e6a6e3a
Guillaume Destuynder <gdestuynder@mozilla.com>
2012-11-28 14:45:52 -0800

    Support for --dport AND --dports (and sport/sports equivalents)
    Note: no test support, examples not fixed!
    
    :dport is now matching --dport (used to match -m multiport --dports)
    :dports is now matching -m multiport --dports

--- lib/puppet/provider/firewall/iptables.rb
+++ lib/puppet/provider/firewall/iptables.rb
@@ -38,7 +38,8 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
   @resource_map = {
     :burst => "--limit-burst",
     :destination => "-d",
-    :dport => "-m multiport --dports",
+    :dports => "-m multiport --dports",
+    :dport => "--dport",
     :gid => "-m owner --gid-owner",
     :icmp => "-m icmp --icmp-type",
     :iniface => "-i",
@@ -53,7 +54,8 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
     :reject => "--reject-with",
     :set_mark => mark_flag,
     :source => "-s",
-    :sport => "-m multiport --sports",
+    :sports => "-m multiport --sports",
+    :sport => "--sport",
     :state => "-m state --state",
     :ctstate => "-m conntrack --ctstate",
     :table => "-t",
@@ -70,7 +72,7 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
   # changes between puppet runs, the changed rules will be re-applied again.
   # This order can be determined by going through iptables source code or just tweaking and trying manually
   @resource_list = [:table, :source, :destination, :iniface, :outiface,
-    :proto, :tcp_flags, :gid, :uid, :sport, :dport, :port, :pkttype, :name, :state, :ctstate, :icmp, :limit, :burst,
+    :proto, :tcp_flags, :gid, :uid, :sports, :sport, :dports, :dport, :port, :pkttype, :name, :state, :ctstate, :icmp, :limit, :burst,
     :jump, :todest, :tosource, :toports, :log_level, :log_prefix, :reject, :set_mark]
 
   def insert
@@ -150,14 +152,14 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
       hash[prop] = Puppet::Util::IPCidr.new(hash[prop]).cidr unless hash[prop].nil?
     end
 
-    [:dport, :sport, :port, :state, :ctstate].each do |prop|
+    [:dports, :dport, :sports, :sport, :port, :state, :ctstate].each do |prop|
       hash[prop] = hash[prop].split(',') if ! hash[prop].nil?
     end
 
     # Our type prefers hyphens over colons for ranges so ...
     # Iterate across all ports replacing colons with hyphens so that ranges match
     # the types expectations.
-    [:dport, :sport, :port].each do |prop|
+    [:dports, :dport, :sports, :sport, :port].each do |prop|
       next unless hash[prop]
       hash[prop] = hash[prop].collect do |elem|
         elem.gsub(/:/,'-')
@@ -270,9 +272,9 @@ Puppet::Type.type(:firewall).provide :iptables, :parent => Puppet::Provider::Fir
 
       args << resource_map[res].split(' ')
 
-      # For sport and dport, convert hyphens to colons since the type
+      # For sports, sport and dports, dport, convert hyphens to colons since the type
       # expects hyphens for ranges of ports.
-      if [:sport, :dport, :port].include?(res) then
+      if [:sport, :dports, :dport, :port].include?(res) then
         resource_value = resource_value.collect do |elem|
           elem.gsub(/-/, ':')
         end
